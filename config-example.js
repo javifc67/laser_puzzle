1f440f47ec6762f1d84025776f2815fb7657750f
javier ferreño upm <javifc67@gmail.com>
2026-02-26 12:14:24 +0100

    update config example

--- config-example.js
+++ config-example.js
@@ -2,7 +2,18 @@
 
 export let ESCAPP_APP_SETTINGS = {
   //Settings that can be specified by the authors
-
+  cols: 6,
+  rows: 6,
+  allowRotate: true,
+  obstacles: [{ label: "A", x: 0, y: 2, angle: 0, allowMove: false }],
+  objects: [{ label: "Aasdasd", type: "SQUARE", x: 2, y: 4, angle: 45, allowMove: true },
+  { color: "yellow", label: "A", type: "SQUARE", x: 2, y: 3, angle: 90, allowMove: true },
+  { label: "A", type: "TRIANGLE", x: 4, y: 4, angle: 30, allowMove: true },
+  { label: "A", type: "TRIANGLE", x: 4, y: 3, angle: 60, allowMove: true },
+  { color: "green", label: "A", type: "TRIANGLE", x: 3, y: 4, angle: 90, allowMove: true },
+  { label: "A", type: "TRIANGLE", x: 3, y: 3, angle: 0, allowMove: true }],
+  transmitter: { label: "A", x: 0, y: 0, angle: 0, allowMove: false },
+  receptor: { label: "A", x: 0, y: 5, allowMove: true },
 
 
   escappClientSettings: {
